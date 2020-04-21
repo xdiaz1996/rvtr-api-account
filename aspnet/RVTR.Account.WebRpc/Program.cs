0d6845c3d3620b4949227b3782ffda6f57347d11
fred belotte <fredbelotte@outlook.com>
2020-04-21 18:17:04 -0500

    add account web rpc project [#172281055]

--- aspnet/RVTR.Account.WebRpc/Program.cs
+++ aspnet/RVTR.Account.WebRpc/Program.cs
@@ -8,20 +8,18 @@ using Microsoft.Extensions.Hosting;
 
 namespace RVTR.Account.WebRpc
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        // Additional configuration is required to successfully run gRPC on macOS.
-        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
