cb2049fabc3e9021a1c965db0b0a4f4916a98dba
fred belotte <fredbelotte@outlook.com>
2020-05-11 17:50:01 -0500

    add support for data context creation [#172282491]

--- aspnet/RVTR.Account.WebApi/Program.cs
+++ aspnet/RVTR.Account.WebApi/Program.cs
@@ -1,5 +1,8 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RVTR.Account.DataContext;
 
 namespace RVTR.Account.WebApi
 {
@@ -11,22 +14,39 @@ namespace RVTR.Account.WebApi
     /// <summary>
     ///
     /// </summary>
-    /// <param name="args"></param>
-    public static void Main(string[] args)
+    /// <returns></returns>
+    public static async Task Main()
     {
-      CreateHostBuilder(args).Build().Run();
+      var host = CreateHostBuilder().Build();
+
+      await CreateDbContextAsync(host);
+      await host.RunAsync();
     }
 
     /// <summary>
     ///
     /// </summary>
-    /// <param name="args"></param>
     /// <returns></returns>
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-      Host.CreateDefaultBuilder(args)
-          .ConfigureWebHostDefaults(webBuilder =>
-          {
-            webBuilder.UseStartup<Startup>();
-          });
+    public static IHostBuilder CreateHostBuilder() =>
+      Host.CreateDefaultBuilder().ConfigureWebHostDefaults(webBuilder =>
+      {
+        webBuilder.UseStartup<Startup>();
+      });
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="host"></param>
+    /// <returns></returns>
+    public static async Task CreateDbContextAsync(IHost host)
+    {
+      using (var scope = host.Services.CreateScope())
+      {
+        var provider = scope.ServiceProvider;
+        var context = provider.GetRequiredService<AccountContext>();
+
+        await context.Database.EnsureCreatedAsync();
+      }
+    }
   }
 }
